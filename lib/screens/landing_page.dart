b10f93e2958fb76a6da5c4edc44eccf066e6d0a1
Mahir Shekh <72331249+Mahir09@users.noreply.github.com>
2021-03-16 09:33:50 +0530

    Login And Register Screen >> First Module Of Event Planner.

---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login_screen/screens/home_page.dart';
+import 'file:///D:/Study/Android%20Studio%20Projects/login_screen/lib/sing_in/sign_in_page.dart';
+import 'package:login_screen/services/auth.dart';
+import 'package:login_screen/services/database.dart';
+import 'package:provider/provider.dart';
+
+class LandingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User user = snapshot.data;
+          if (user == null) {
+            return SignInPage.create(context);
+          }
+          return Provider<Database>(
+            create: (_) => FirestoreDatabase(uid: user.uid),
+            child: HomePage(),
+          );
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
